420424d2670ea18d30f70dc288ffa8b99f7412b7
Ana-ku1011 <61750637+Ana-ku1011@users.noreply.github.com>
2020-03-17 17:01:48 -0600

    Add files via upload

---
+++ main.c
@@ -0,0 +1,7 @@
+<stdios.h>
+git
+git diff // diferencia de dos archivos//
+git add /// hacer correr un archivo//
+git commit// crear un archivo//
+git init // lo que se encuentra en el directorio/
+git status// para ver el estado del archivo//
